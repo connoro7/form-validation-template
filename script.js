b0d10a75e11cd9022a8a2af832a2e709d7908596
Connor Dillon <connor@codewithconnor.com>
2020-09-09 17:17:53 -0700

    :wrench: Fixed script.js reverting to previous commit

--- script.js
+++ script.js
@@ -20,9 +20,13 @@ function showSuccess(input) {
 }
 
 //$ Regex check for valid email
-function isValidEmail(email) {
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  return re.test(String(email).toLowerCase())
+  if (re.test(input.value.trim())) {
+    showSuccess(input)
+  } else {
+    showError(input, 'Email is not valid')
+  }
 }
 
 /**
@@ -40,6 +44,35 @@ function checkRequired(inputArr) {
   })
 }
 
+//$ Check input length
+/**
+ * @description
+ * @param {string} input String to be evaluated for char length
+ * @param {number} minLength Minumum allowed length of input
+ * @param {number} maxLength Maximum allowed length of input
+ * @returns
+ */
+function checkLength(input, minLength, maxLength) {
+  if (input.value.length < minLength) {
+    showError(input, `${capitalizeWord(input)} must be at least ${minLength} characters`)
+  } else if (input.value.length >= maxLength) {
+    showError(input, `${capitalizeWord(input)} cannot be longer than ${maxLength} characters`)
+  } else {
+    showSuccess(input)
+  }
+}
+
+//$ Check for matching passwords
+/**
+ * @description Validates if Password and Password Confirmation fields match
+ * @param {string} input1 Password field
+ * @param {string} input2 Password confirmation field
+ */
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match')
+  }
+}
 /**
  * @description Takes in a string and uppercases the first letter only
  * @param {string} input Form field input name
@@ -57,4 +90,8 @@ form.addEventListener('submit', function (e) {
   e.preventDefault()
 
   checkRequired([username, email, password, password2])
+  checkLength(username, 3, 30)
+  checkLength(password, 6, 63)
+  checkEmail(email)
+  checkPasswordsMatch(password, password2)
 })
